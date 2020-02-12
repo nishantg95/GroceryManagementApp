76ee274ab21b4cb594bf91045a7462d473976153
Grover <nishant.b.grover@accenturefederal.com>
2020-02-11 18:06:15 -0600

    Adding Angular components for welcome, inventory, and repoItem pages

---
+++ GroceryManagementAppFrontv2/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ItemsComponent } from './items/items.component';
+import { RepoItemsComponent } from './repo-items/repo-items.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ItemsComponent,
+    RepoItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ GroceryManagementAppFrontv2/src/app/repo-items/repo-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-items',
+  templateUrl: './repo-items.component.html',
+  styleUrls: ['./repo-items.component.css']
+})
+export class RepoItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
